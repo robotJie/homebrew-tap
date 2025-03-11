cea4b49e1f553802569b1d1687384798645f3827
robotJie <robotjie@icloud.com>
2025-03-11 17:16:12 +0800

    bump version:v0.0.7

--- Formula/m/mdns-reflector-go.rb
+++ Formula/m/mdns-reflector-go.rb
@@ -1,15 +1,21 @@
 class MdnsReflectorGo < Formula
     desc "Simple mDNS reflector written in golang"
     homepage "https://github.com/robotJie/mdns_reflector_go"
-    url "https://github.com/robotJie/mdns_reflector_go/archive/refs/tags/v0.0.5.tar.gz"
-    version "0.0.5"
-    sha256 "48b6f47e15e767b5fa4ee76b519a451a7aeee4e3f9f445dc3d8df24895d91d42"
+    url "https://github.com/robotJie/mdns_reflector_go/archive/refs/tags/v0.0.7.tar.gz"
+    sha256 "bc713869b11809b5fa2ac68b6516d79d4e51cd78e731cc633b67c7e681889087"
     license "MIT"
   
     depends_on "go" => :build
   
     def install
       system "go", "build", *std_go_args(ldflags: "-s -w"), "main.go"
+    end
+
+    service do
+        run [opt_bin/"mdns-reflector-go"]
+        keep_alive true
+        log_path var/"log/mdns-reflector-go.log"
+        error_log_path var/"log/mdns-reflector-go.log"
     end
   
     test do
